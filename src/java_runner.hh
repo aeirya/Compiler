e1a6fc73b8479ed964320759cff1859bc4cc0a67
aeirya <aeirya@gmail.com>
2022-08-18 13:48:23 +0430

    update javarunner

--- src/java_runner.hh
+++ src/java_runner.hh
@@ -1,11 +1,15 @@
+#include <stdlib.h>
 #include <string>
 
 void runJava(char* classpath, char* classname) {
     std::stringstream ss;
     ss << "java -cp " << classpath << " " << classname << std::endl;
-    program(ss.str());
+    int status = system(ss.str().c_str());
+    if (status == 0) {
+        std::cout << "running java" << std::endl;
+    }
 }
 
 void runJavaCompiler() {
-    runJava("../java/target/classes", "Main");
+    runJava("../java/target/classes", "Other");
 }
